f7e656d75354e6e071a64e3f647b4cbd646a034d
Aditya Prajapati <adityaprajapati139@gmail.com>
2025-05-19 19:08:39 +0530

    updated some files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // let name = "Aditya";
 function App() {
+  const [mode,setMode] = useState('light'); // state variable which tell the dark mode is enabled or not
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+    }else{
+      setMode('light');
+    }
+  }
   return (
     <>
-    <Navbar aboutText="About"/>
+    <Navbar title="Library" aboutText="About" mode={mode} toggleMode={toggleMode}/>
     <div className="container my-3">
       <TextForm heading="Enter the text to anaylyze below"/> 
       {/* <About/> */}
